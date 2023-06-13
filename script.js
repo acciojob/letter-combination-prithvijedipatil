36db0033f4d3ec270f36e1fa262f6c75e65b19dc
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-13 18:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function letterCombinations(input_digit) {
   //Complete the function
+
+	/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if(!digits.length) return [];
+    let map = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z'],
+    }
+
+    let res=[];
+    function b(s,i){
+        if(s.length==digits.length){
+            res.push(s);
+            return
+            
+        }
+         for(let x of map[digits[i]]){
+            b(s+x,i+1);
+        }
+    }
+        b("",0);
+        return res;
+    
+};
 }
 
 module.exports = letterCombinations;
